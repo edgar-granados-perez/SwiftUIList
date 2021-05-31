d7e857f379b0a25ac7c99281bd91793d39ae38ea
holounix <60943198+holounix@users.noreply.github.com>
2021-05-31 12:18:37 -0500

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Edgar Granados-Perez on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
